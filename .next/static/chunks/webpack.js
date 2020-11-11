082f5ffa684382ec85636475cda9488db1fc7deb
Bruno Alcântara <brunoalcantarajc@gmail.com>
2020-11-10 23:29:14 -0300

    Remoçao do typescript

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e580b83ab61eefbf5628";
+/******/ 	var hotCurrentHash = "f256cbfcf9be30736987";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
